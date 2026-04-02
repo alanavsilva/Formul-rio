231bbdb27d74b0f04ef07efe3677a115024c4c00
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-02 12:12:11 -0300

    Add- Tive que excluir commits antigos para iniciar repositório novo, comecei javascript

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function validacao(){
+function validacao() {
+
+    //captações
+
+    let nome = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let cpf = document.getElementById("cpf").value;
+    let idade = document.getElementById("idade").value;
+    let cidade = document.getElementById("cidade").value;
+
+
+
+
+    // validações
+
+
+
+    if (nome.length < 3) {
+        alert("Nome deve ter no mínimo 3 caracteres");
+    }
+
+
 
 }
