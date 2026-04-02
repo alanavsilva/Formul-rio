36a1773a835ecf37303f95d76f84490288ad79bc
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-02 12:15:48 -0300

    Add- captações feitas

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ function validacao() {
 
     //captações
 
-    let nome = document.getElementById("name").value;
+    let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
     let telefone = document.getElementById("telefone").value;
     let cpf = document.getElementById("cpf").value;
     let idade = document.getElementById("idade").value;
     let cidade = document.getElementById("cidade").value;
+    let tipo = document.getElementById("tipo").value;
+    let quintal = document.getElementById("quintal").value;
+    let antes = document.getElementById("antes").value;
+    let horas_sozinho = document.getElementById("horas_sozinho").value;
+    let motivo = document.getElementById("motivo").value;
+    let termo = document.getElementById("termo").value;
 
 
 
